51370b0756221d74c65da8ac26a0440d4fd85348
Jonathan Adams <jwadams@eros-os.com>
2007-09-10 11:43:48 -0400

    Fix problems in by-hand kernel tests
        minor Makefile problem
        Update description of LDCAP/STCAP tests
        s/CAP_REG/REG_CAPLOC/g
        s/_NEW_INV/_INV/g
        missing comma in null_sendonly-data.c

--- src/sys/arch/i386/test-image/by-hand/ipc-data.c
+++ src/sys/arch/i386/test-image/by-hand/ipc-data.c
@@ -5,7 +5,7 @@
 const InvParameterBlock_t ipc_parameter_block = {
   .pw[0] = IPW0_RP|IPW0_CO|IPW0_RC|IPW0_SP+IPW0_SC+IPW0_MAKE_LSC(0)+IPW0_MAKE_LDW(1)+sc_InvokeCap,
   .pw[1] = OC_coyotos_Cap_getType,
-  .u.invCap = CAP_REG(2),
-  .sndCap[0] = CAP_REG(1),
+  .u.invCap = REG_CAPLOC(2),
+  .sndCap[0] = REG_CAPLOC(1),
   .epID = 0,
 };
--- src/sys/arch/i386/test-image/by-hand/klog-data.c
+++ src/sys/arch/i386/test-image/by-hand/klog-data.c
@@ -5,11 +5,11 @@
 #define MYSTR "This is a test of the Kernel Log Cap!\n"
 
 /* This currently uses an open wait. */
-_NEW_INV_coyotos_KernLog_log ipc_parameter_block = { .in = {
+_INV_coyotos_KernLog_log ipc_parameter_block = { .in = {
   ._icw = IPW0_RP|IPW0_CO|IPW0_RC|IPW0_SP+IPW0_SC+IPW0_MAKE_LSC(0)+IPW0_MAKE_LDW(4)+sc_InvokeCap,
   ._opCode = OC_coyotos_KernLog_log,
-  ._invCap = CAP_REG(2),
-  ._replyCap = CAP_REG(1),
+  ._invCap = REG_CAPLOC(2),
+  ._replyCap = REG_CAPLOC(1),
   .msg = {.max = 256, .len = sizeof (MYSTR), .data = 0 },
   ._sndPtr = &ipc_parameter_block.in._indirect[0],
   ._sndLen = sizeof(MYSTR),
