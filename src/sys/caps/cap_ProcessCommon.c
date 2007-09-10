5af3eea20630720c55e2a4b5cd6bd9507aaf0404
Jonathan Adams <jwadams@eros-os.com>
2007-09-10 16:54:37 -0400

    Fix #1: Handler update on faulted process

--- src/sys/caps/cap_ProcessCommon.c
+++ src/sys/caps/cap_ProcessCommon.c
@@ -178,6 +178,11 @@ void cap_ProcessCommon(InvParam_t *iParam)
       capability *pSlot = 0;
       switch (slot) {
       case coyotos_Process_cslot_handler:
+	/* We may be asleep waiting for our handler to become available.
+	 * Unblock ourselves just in case, to give a chance at the new
+	 * handler.
+	 */
+	sq_Unsleep(p);
 	pSlot = &p->state.handler;
 	break;
       case coyotos_Process_cslot_addrSpace:
