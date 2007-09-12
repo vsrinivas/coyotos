975ec93bb8cb7a9d75ce6afa765186ce1c271c75
Jonathan Adams <jwadams@eros-os.com>
2007-09-12 16:32:05 -0400

    Add GPL copyrights.

--- src/base/test/testHandler/testHandler.c
+++ src/base/test/testHandler/testHandler.c
@@ -1,6 +1,33 @@
-/* Example template for processing the following interfaces:
-    coyotos.MemoryHandler
-    coyotos.ProcessHandler
+/*
+ * Copyright (C) 2007, The EROS Group, LLC
+ *
+ * This file is part of the Coyotos Operating System.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/** @file
+ * @brief Handler test case.
+ *
+ * This is a simple test case for checking whether the kernel Handler
+ * invocation paths are working.
+ *
+ * It responds to both the MemoryHandler and ProcessHandler invocations,
+ * and just prints a message to the log and goes back to waiting for requests.
+ *
+ * No attempt is made to do *anything* to the failing processes.
  */
 
 #include <coyotos/capidl.h>
